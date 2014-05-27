92dbb586b7388019ee6f281a30b9d8216badf19d
mudler <mudler@dark-lab.net>
2014-05-27 14:23:16 +0200

    minor fixes, bumping release version

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export SABAYON_MOLECULE_HOME=$(pwd)"/molecules"
 export SABAYON_RELEASE="1.0"
-export ISO_TAG="14.01"
+export ISO_TAG="14.05"
+molecule specs/spike-pentesting-86.spec
 molecule specs/spike-pentesting-64.spec
