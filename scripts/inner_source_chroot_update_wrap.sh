9e57974a0e0ce50242220882baa9c884322efe09
mudler <mudler@dark-lab.net>
2014-06-13 22:23:08 +0200

    WTF

---
+++ scripts/inner_source_chroot_update_wrap.sh
@@ -0,0 +1,9 @@
+#echo '192.99.32.76 repository.spike-pentesting.org' >>/etc/hosts
+
+echo '[spike]
+desc = Spike Pentesting Sabayon Repository
+repo = http://repository.spike-pentesting.org#bz2
+enabled = true
+pkg = http://repository.spike-pentesting.org
+' >> /etc/entropy/repositories.conf.d/spike
+
