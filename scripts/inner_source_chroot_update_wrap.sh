0111d987d69cfc537952dc7cb6363da17b463b0b
mudler <mudler@dark-lab.net>
2014-06-14 23:28:11 +0200

    ooops

--- scripts/inner_source_chroot_update_wrap.sh
+++ scripts/inner_source_chroot_update_wrap.sh
@@ -7,5 +7,5 @@ enabled = true
 pkg = http://repository.spike-pentesting.org
 ' >> /etc/entropy/repositories.conf.d/spike
 
-rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/profiles/" "/usr/portage/licenses/"
+rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/licenses/"
 ls /usr/portage/licenses -1 | xargs -0 > /etc/entropy/packages/license.accept
