9ae67a82091e1ed7372d30fbf27f3e31ef5082d3
mudler <mudler@dark-lab.net>
2014-06-14 11:19:48 +0200

    modified some

--- scripts/remaster_generic_inner_chroot_script_after_wrap.sh
+++ scripts/remaster_generic_inner_chroot_script_after_wrap.sh
@@ -1,19 +1,20 @@
+
 if [ "${1}" = "lxde" ]; then
-
+	echo "LXDE build!"
 elif [ "${1}" = "mate" ]; then
-
+	echo "Mate build!"
 elif [ "${1}" = "e17" ]; then
 	genmenu.py -e
 elif [ "${1}" = "xfce" ]; then
-
+	echo "XFCE build!"
 elif [ "${1}" = "fluxbox" ]; then
-
+	echo "FLUXBOX build!"
 elif [ "${1}" = "gnome" ]; then
-
+	echo "GNOME build!"
 elif [ "${1}" = "xfceforensic" ]; then
-
+	echo "xfceforensic build!"
 elif [ "${1}" = "kde" ]; then
-
+	echo "KDE build!"
 elif [ "${1}" = "awesome" ]; then
-
+	echo "AWESOME build!"
 fi
